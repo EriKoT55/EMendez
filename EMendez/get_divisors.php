084302090eb2ae2d3f7bdaae14f33d7c8f2260ab
erik <emendezmartin@iessonferrer.net>
2021-09-29 19:54:46 +0200

    asdfa

--- EMendez/get_divisors.php
+++ EMendez/get_divisors.php
@@ -1,20 +1,34 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
 <head>
     <title>Get divisors</title>
 </head>
 <body>
-    <form>
+    <form method="post" action="get_divisors.php">
+        <label>
+            Number:
+            <input type="text" name="num">
 
+        </label type="submit">
 
-
+            <input type="submit" name="enviar_num">
 
     </form>
 <div>
-
     <?php
-    ?>
 
+        if(isset($_POST["num"])) {
+            $num = intval($_POST["num"]);
+
+            for($i=0;$i<$num;$i++){
+                if($num/$i==0){
+                    echo $i;
+                }
+            }
+
+        }
+
+    ?>
 </div>
 
 </body>
