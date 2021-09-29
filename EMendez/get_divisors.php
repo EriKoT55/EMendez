136b534895dc3ad38450ef5b78362b9ae01e680d
erik <emendezmartin@iessonferrer.net>
2021-09-29 19:33:23 +0200

    Initial commit

---
+++ EMendez/get_divisors.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Get divisors</title>
+</head>
+<body>
+    <form>
+
+
+
+
+    </form>
+<div>
+
+    <?php
+    ?>
+
+</div>
+
+</body>
+</html>
+
