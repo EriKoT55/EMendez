a4c7944b8bc30e649fd6dc623916d12d01000d38
erik <emendezmartin@iessonferrer.net>
2021-10-01 17:29:57 +0200

    asdfa

--- EMendez/pruebas.php
+++ EMendez/pruebas.php
@@ -18,9 +18,7 @@
     <?php
         $array=[];
         if(isset($_POST["num"])) {
-            //isset evalua si una variable esta definida o no.
-            //La variable de dentro POST busca la parte del formulario de num y cuando tu le metes el numero
-            //lo que hace es ver que se ejecuta al enviar el formulario
+
             $num = intval($_POST["num"]);
 
             for($i=1;$i<=$num;$i++){
@@ -28,7 +26,7 @@
                 if($num%$i==0){
                     echo "<br> Divisible por: ".$i.'<br>';
                     $array[]=$i;
-
+                    return $array;
                 }
             }
             var_dump($array);
