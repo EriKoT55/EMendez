17f29eea026ec3b3569b92694d1c4c75f683e866
erik <emendezmartin@iessonferrer.net>
2021-10-05 20:52:24 +0200

    asdfa

--- EMendez/numPerfectos.php
+++ EMendez/numPerfectos.php
@@ -26,16 +26,17 @@
     function isPerfectNum($num){
        // es igual a la suma de sus divisores
 
-        $j=0;
-        $i=1;
+        $j=0; // contador el cual guarda cuantos numeros perfectos voy sacando
+        $i=1; // contador el cual cogere para mostrarme los numeros perfectos
 
         while($j<$num){
-            $perfecto=getDivisors($i);
-            $sum=array_sum($perfecto);
+            $perfecto=getDivisors($i); // guardo la funcion anterior en una variable
+            $sum=array_sum($perfecto); //sumo lo que hay en cada posicion de un array y lo guardoen una varable
 
-            if($sum==$i){
-                echo $i.'<br>';
-                $j++;
+            if($sum==$i){ // si la suma de los divisores es igual a la variable i
+                // por ejemplo si sum=6 == a i=6
+                echo $i.'<br>'; // devuelve las $i que son numeros perfectos
+                $j++;// Me guarda en j el numero de numeros perfectos
             }
             $i++;
 
@@ -47,7 +48,7 @@
         $num = intval($_POST["num"]);
 
         echo 'Los '.$num.' primeros numeros perfectos <br>';
-        isPerfectNum($num);
+        isPerfectNum($num); // muestra los numeros perfectos
 
 
     }
