9a9646cccf7e4dbbd7f8c249fcf7c09bb59d437a
EriKoT <erik55@outlook.es>
2021-10-07 12:07:04 +0200

    dfg

--- Asteriscos.php
+++ Asteriscos.php
@@ -1,7 +1,58 @@
-<?php
+<html lang="es">
+<head>
+    <title>Arbol de Asteriscos</title>
+</head>
+<body>
+<form method="post" action="Asteriscos.php">
+    <label>
+        Number:
+        <input type="text" name="num"/>
+    </label>
+    <input type="submit"/>
+</form>
+<div  echo '<span style="background-color:black"></span>';>
+    <?php
 
-for ($i=1;$i<=10;$i++){
-    echo 'asdfasdf'.$i;
-}
+    function setArbolAsteriscos($num){
 
-?>
+
+   /*1*/ for($filas=0;$filas<=$num;$filas++){
+            echo '<br>';
+
+      /*3*/ for($j=$num;$j>$filas;$j--){// Asteriscos lateral izquierdo
+
+                echo '*';
+
+            }
+
+      /*2*/ for($i=0;$i<=$filas;$i++){// Asteriscos central derecho
+
+                echo '<span style="color:darkorange">*</span>';
+
+
+
+
+            }
+            /*5*/  for($i=0;$i<$filas;$i++){
+                echo '<span style="color: darkorange">*</span>';// Asteriscos central izquierdo
+            }
+      /*4*/ for($z=$num;$z>$filas;$z--){// Asteriscos lateral derecho
+
+                echo '*';
+
+            }
+    }
+
+    }
+
+
+    if (isset($_POST["num"])) {
+        $num = intval($_POST["num"]);
+
+        setArbolAsteriscos($num);
+
+    }
+    ?>
+</div>
+</body>
+</html>
