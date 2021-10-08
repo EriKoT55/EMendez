d173a9b837a8d3f68361d7aee6fa1c6204b726c5
erik <emendezmartin@iessonferrer.net>
2021-10-08 16:45:43 +0200

    asdfa

--- EMendez/Contraseña.php
+++ EMendez/Contraseña.php
@@ -7,6 +7,7 @@
         }
     </style>
 </head>
+<center>
 <body>
 
 <form method="post" action="Contraseña.php">
@@ -25,13 +26,47 @@
 
     }
 
+    function TimeforCracking($password){
+
+
+
+    }
+
+    function changeColor($password){
+        // son muchos if/ else if
+        if(strlen($password)==0) {
+            echo '<style> body{
+                            background-color: deepskyblue;
+                        }   </style>';
+        }else if(strlen($password)>0 && srtlen($password)<= 4){
+            echo '<style> body{
+                            background-color: red;
+                        }  </style>';
+        }else if(strlen($password)>=5 && srtlen($password)<= 8){
+            echo '<style> body{
+                            background-color: orange;
+                        }  </style>';
+        }else if(strlen($password)>=9 && srtlen($password)<= 11){
+            echo '<style> body{
+                            background-color: yellow;
+                        }  </style>';
+        }else if(strlen($password)>=12){
+            echo '<style> body{
+                            background-color: greenyellow;
+                        }  </style>';
+        }
+
+    }
 
     if (isset($_POST["password"])) {
         $password = intval($_POST["password"]);
+
+        changeColor($password);
 
 
     }
     ?>
 </div>
 </body>
+</center>
 </html>
