d1d045ba5132706b84511bc973cf58cbc41f911c
erik <emendezmartin@iessonferrer.net>
2021-10-14 17:29:36 +0200

    asdfa

--- EMendez/Contrasenya.php
+++ EMendez/Contrasenya.php
@@ -1,20 +1,31 @@
 <html lang="es">
 <head>
-    <title>How Secure Is My Password</title>
+    <title>How Secure Is My Password?</title>
     <style>
         body{
             background-color: aqua;
+            text-align: center;
+            padding: 200px;
+        }
+        .password{
+            border: 1px solid rgba(0, 0, 0, 0);
+            background-color: rgba(170, 167, 167, 0.638);
+            height: 60px;
+        }
+        .password,placeholder{
+            text-align: center;
         }
     </style>
 </head>
 <body>
 
 <form method="post" action="Contrasenya.php">
+    <h2>How Secure is my password?</h2>
     <label>
-        Password:
-        <input type="password" name="password" placeholder="Enter your password"/>
+
+        <input type="password" class="password" name="password" size="50"  placeholder="Enter your password"/>
     </label>
-    <input type="submit"/>
+
 </form>
 <div>
     <?php
@@ -29,12 +40,16 @@
 
         //Sacar meses
         $mesesforCracking=($anyosforCracking - floor($anyosforCracking))*12;
+
         //Sacar dias
-        $diasforCraking=($mesesforCracking- floor($mesesforCracking))*365;
+        $diasforCraking=($mesesforCracking- floor($mesesforCracking))*30;
+
         //Sacar horas
         $horasforCraking=($diasforCraking-floor($diasforCraking))*24;
+
         //Sacar minutos
         $minutosforCraking=($horasforCraking-floor($horasforCraking))*60;
+
         //Sacar segundos
         $segundosforCraking=($minutosforCraking-floor($minutosforCraking))*60;
 
@@ -48,12 +63,11 @@
             echo "<body style='background-color: aqua' ></body>";
         }else if($numchar>0 && $numchar<=4){
             echo "<body style='background-color: red' ></body>";
-
         }else if($numchar>=5 && $numchar<=8){
             echo "<body style='background-color: orange' ></body>";
         }else if($numchar>=9 && $numchar<=12){
             echo "<body style='background-color: yellow' ></body>";
-        }else if($numchar>13){
+        }else if($numchar>=13){
             echo "<body style='background-color: lawngreen' ></body>";
         }
 
