fced32a9b5c268a4980d5cdbe64cc047f84ae9d2
EriKoT <erik55@outlook.es>
2021-10-19 23:00:43 +0200

    dfg

--- EMendez/world.php
+++ EMendez/world.php
@@ -6,23 +6,29 @@ function getUnsortedCities($world){
     //TODO: Return an array of cities without any kind of sort.
     //NOTES 1: You receive a world multidimensional array, you can view it's content with var_dump() function.
     //NOTES 2:You CAN'T use any sorting PHP built-in function.
-    for($i=0;$i<count($world);$i++){
+    $cities = [];
 
+    foreach ($world as $country){
+        foreach($country["Cities"] as $city){
+            $cities[]=$city;
+        }
+    }
+    return $cities;
+
+    /*for($i=0;$i<count($world);$i++){
         for($j=0;$j<count($world);$j++){
-            if($world[$i]["Population"]<$world[$j]["Population"] ){
-
+            if($world[$i]["Cities"]<$world[$j]["Cities"] ){
                 $aux=$world[$i];
                 $world[$i]=$world[$j];
                 $world[$j]=$aux;
-
             }
         }
-
     }
     echo "<br>";
     echo "<pre>";
     var_dump($world);
     echo "</pre>";
+    */
 }
 
 function getSortedCitiesByPopulation($cities){
@@ -55,7 +61,7 @@ function getSortedCitiesByPopulation($cities){
 <table>
     <thead>
     <tr>
-        <th colspan="6">Cities of the world (<?php //TODO: Logic to print the number of cities. ?>)</th>
+        <th colspan="6">Cities of the world (<?php count() ?>)</th>
     </tr>
     <tr>
         <th colspan="3">Unsorted cities</th>
@@ -73,7 +79,16 @@ function getSortedCitiesByPopulation($cities){
     <tbody>
     <?php
     //TODO: Logic to print the table body.
-    getUnsortedCities($world);
+    $ciudades=getUnsortedCities($world);
+
+        for($i=0;$i<count($world);$i++){
+            echo "<tr>";
+                for($j=0;$j<count($world);$j++){
+
+                }
+            echo "</tr>";
+        }
+
     ?>
     </tbody>
 </table>
