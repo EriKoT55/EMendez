036238b9d307cff387f347d9c3bacf046ffabd67
erik <emendezmartin@iessonferrer.net>
2021-10-22 17:13:20 +0200

    sdf

--- EMendez/Tema2_POO/rickmorthy2.php
+++ EMendez/Tema2_POO/rickmorthy2.php
@@ -5,24 +5,52 @@ $api_url = "https://dawsonferrer.com/allabres/apis_solutions/rickandmorty/api.ph
 $characters = json_decode(file_get_contents($api_url . "characters"), true);
 $episodes = json_decode(file_get_contents($api_url . "episodes"), true);
 $locations = json_decode(file_get_contents($api_url . "locations"), true);
+var_dump($characters);
 
+class Character{
 
-class rickmorthy2{
-
-    public $characters;
+    public $id, $name, $status, $species, $type, $gender, $origin, $location, $image, $created, $episodes;
 
     //Metodos
-    function setCharacters($characters){
+    function setCharacter($id, $name, $status, $species, $type, $gender, $origin, $location, $image, $created, $episodes){//me da los valores
 
-        $this->characters=$characters;
+        $this->id=$id;
+        $this->name=$name;
+        $this->status=$status;
+        $this->species=$species;
+        $this->type=$type;
+        $this->gender=$gender;
+        $this->origin=$origin;
+        $this->location=$location;
+        $this->image=$image;
+        $this->created=$created;
+        $this->episodes=$episodes;
+
+    }
+    function getCharacter(){// me recoge los valores
+
+        return $this->id;
+        return $this->name;
+        return $this->status;
+        return $this->species;
+        return $this->type;
+        return $this->gender;
+        return $this->origin;
+        return $this->location;
+        return $this->image;
+        return $this->created;
+        return $this->episodes;
 
     }
 
-
 }
 
-$personajes = new rickmorthy2();
+$personajes = new rickmorthy2();//objeto vacio
 
-var_dump($personajes);
+for($i=0;i<count($personajes);$i++){
+
+    $personajes[$i]->setCharacters[$characters[$i]];
+
+}
 
 ?>
