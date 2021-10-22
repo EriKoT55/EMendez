501affb0038c7316583f8a23da379e5e73cbed59
erik <emendezmartin@iessonferrer.net>
2021-10-22 15:46:44 +0200

    sdf

--- EMendez/Tema2_POO/rickmorthy2.php
+++ EMendez/Tema2_POO/rickmorthy2.php
@@ -1,1 +1,6 @@
+$characters = json_decode(file_get_contents($api_url . "characters"), true);
+$episodes = json_decode(file_get_contents($api_url . "episodes"), true);
+$locations = json_decode(file_get_contents($api_url . "locations"), true);
 <?php
+
+?>
