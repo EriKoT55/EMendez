f44492f26fc9843268be29b75c1a59f1a0c94314
EriKoT <erik55@outlook.es>
2021-11-07 21:22:39 +0100

    dfg

--- EMendez/Tema2_POO/Elecciones/Partidos.php
+++ EMendez/Tema2_POO/Elecciones/Partidos.php
@@ -1,72 +1,108 @@
 <?php
 
-    class Partidos{
+class Partidos
+{
 
-        private $id,$name,$acronym,$logo,$colour;
-
-        public function __construct($id, $name, $acronym, $logo, $colour){
-            $this->id = $id;
-            $this->name = $name;
-            $this->acronym = $acronym;
-            $this->logo = $logo;
-            $this->colour = $colour;
-        }
+    private $id, $name, $acronym, $logo, $colour, $votos, $escanyos;
 
 
-        public function getId(){
-            return $this->id;
-        }
-
-
-        public function setId($id){
-            $this->id = $id;
-            return $this;
-        }
-
-
-        public function getName(){
-            return $this->name;
-        }
-
-
-        public function setName($name){
-            $this->name = $name;
-            return $this;
-        }
-
-
-        public function getAcronym(){
-            return $this->acronym;
-        }
-
-
-        public function setAcronym($acronym){
-            $this->acronym = $acronym;
-            return $this;
-        }
-
-        public function getLogo(){
-            return $this->logo;
-        }
-
-
-        public function setLogo($logo){
-            $this->logo = $logo;
-            return $this;
-        }
-
-        public function getColour(){
-            return $this->colour;
-        }
-
-
-        public function setColour($colour){
-            $this->colour = $colour;
-            return $this;
-        }
-
-
-
+    public function __construct($id, $name, $acronym, $logo, $colour)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->acronym = $acronym;
+        $this->logo = $logo;
+        $this->colour = $colour;
     }
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+
+    public function getAcronym()
+    {
+        return $this->acronym;
+    }
+
+
+    public function setAcronym($acronym)
+    {
+        $this->acronym = $acronym;
+        return $this;
+    }
+
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+
+
+    public function setLogo($logo)
+    {
+        $this->logo = $logo;
+        return $this;
+    }
+
+    public function getColour()
+    {
+        return $this->colour;
+    }
+
+
+    public function setColour($colour)
+    {
+        $this->colour = $colour;
+        return $this;
+    }
+
+
+    public function getVotos()
+    {
+        return $this->votos;
+    }
+
+
+    public function setVotos($votos)
+    {
+        $this->votos = $votos;
+    }
+
+
+    public function getEscanyos()
+    {
+        return $this->escanyos;
+    }
+
+
+    public function setEscanyos($escanyos)
+    {
+        $this->escanyos = $escanyos;
+    }
+
+
+}
 
 ?>
--- EMendez/Tema2_POO/Elecciones/main.php
+++ EMendez/Tema2_POO/Elecciones/main.php
@@ -10,6 +10,7 @@ $partidos = json_decode(file_get_contents($api_url . "parties"), true);
 $provincias = json_decode(file_get_contents($api_url . "districts"), true);
 $resultados = json_decode(file_get_contents($api_url . "results"), true);
 
+
 /*echo "<br>";
 echo "<pre>";
 var_dump($provincias);
@@ -45,93 +46,17 @@ $divisor = 0;
 
 //Duplicar objeto results, para trabajar en el duplicado e ir dividiendo los votos para sacar los escaños despues pasar los escaños al original
 $resultsDuplicate_obj = $results_obj;
-
-echo "<br>";
-echo "<pre>";
-var_dump($resultsDuplicate_obj);
-echo "</pre>";
-
-//Funcion para sacar delegaciones
-function Delegados($provincias_obj, $provinciaSelected)
+function tabla($resultadosProvincias,$results_obj)
 {
 
-    for ($h = 0; $h < count($provincias_obj); $h++) {
-        if ($provincias_obj[$h]->getName() == $provinciaSelected ) {
-            return $provincias_obj[$h]->getDelegates();
-        }
-    }
-
-}
-
-function ResultsFiltrados($resultsDuplicate_obj,$provinciaSelected){
-$resultsFiltrados=[];
-    for($i=0;$i<count($resultsDuplicate_obj);$i++){
-
-        if($resultsDuplicate_obj[$i]->getDistrito() == $provinciaSelected){
-            $resultsFiltrados[$i]=$resultsDuplicate_obj[$i];
-        }
-
-    }
-    return $resultsFiltrados;
-}
-
-function Escanyos()
-{
-
-    global $results_obj;
-    global $provincias_obj;
-    global $resultsDuplicate_obj;
-    $resultadosFiltrados=ResultsFiltrados($resultsDuplicate_obj,$_GET["provincia"]);
-
-
-    //Ordeno los votos de mayor a menor,
-    /*for ($k = 0; $k < count($resultadosFiltrados); $k++) {
-        for ($i = $k; $i < count($resultadosFiltrados); $i++) {
-            if ($resultadosFiltrados[$k]->getVotos() < $resultadosFiltrados[$i]->getVotos()) {
-                $aux = $resultadosFiltrados[$k];
-                $resultadosFiltrados[$k] = $resultadosFiltrados[$i];
-                $resultadosFiltrados[$i] = $aux;
-            }
-        }
-    }*/
-// Hay un momento en el cual se pierden datos y solo muestra los de madrid buscar, en la duplicacion no esta el problema
-    // ya lo he comprobado y se pasan todos los datos al duplicado.
-    for($j=0;$j<count($resultadosFiltrados);$j++){
-        echo "<br>";
-        echo "<pre>";
-        var_dump($resultadosFiltrados[$j]->getVotos());
-        echo "</pre>";
-    }+
-
-
-
-    $divisor = 2;
-
-
-// De una provincia cojo el partido que mas votos tiene, a este lo divido entre dos y le sumo un escaño, despues miro quien es el que mas votos tiene y hago la misma operación, si
-// ya tiene un escaño pero sigue siendo el que mas votos tiene dividir entre 3 y sumar otro escaño... asi sucesivamente. Hasta repartir todas las delegaciones en cada provincia.
-// problema no se como automatizar esto que acabo de escribir
-   $delegados=Delegados($provincias_obj, $_GET["provincia"]);
-    /*for($k=0;$k<count($delegados);$k++){
-        for($i=0;$i<count($resultadosFiltrados);$i++){
-
-            $resultadosFiltrados[0]/$divisor;
-
-        }
-    }*/
-
-}
-
-function tabla($resultadosProvincias)
-{
-    global $results_obj;
     echo "<table>";
     echo "<tr>";
     echo "<th>Circumscripción</th>";
     echo "<th>Partido</th>";
     echo "<th>Votos</th>";
     echo "<th>Escaños</th>";
-    echo "</tr>";echo "<tr>";
+    echo "</tr>";
+    echo "<tr>";
     for ($i = 0; $i < count($results_obj); $i++) {
 
         if ($results_obj[$i]->getDistrito() == $resultadosProvincias) {
@@ -139,56 +64,168 @@ function tabla($resultadosProvincias)
             echo "<td>" . $results_obj[$i]->getDistrito() . "</td>";
             echo "<td>" . $results_obj[$i]->getPartidos() . "</td>";
             echo "<td>" . $results_obj[$i]->getVotos() . "</td>";
+            echo "<td>" . $results_obj[$i]->getEscanyos() . "</td>";
             echo "</tr>";
         }
 
     }
     echo "</table>";
 }
+/*
+echo "<br>";
+echo "<pre>";
+var_dump($resultsDuplicate_obj);
+echo "</pre>";
+*/
+
+//Funcion para sacar delegaciones
+function Delegados($provincias_obj, $provinciaSelected)
+{
+
+    for ($h = 0; $h < count($provincias_obj); $h++) {
+        if ($provincias_obj[$h]->getName() == $provinciaSelected) {
+            return $provincias_obj[$h]->getDelegates();
+        }
+    }
+
+}
+//Filtrar resultados por provincia seleccionada
+function ResultsFiltrados($resultsDuplicate_obj, $provinciaSelected)
+{
+    $resultsFiltrados = [];
+    for ($i = 0; $i < count($resultsDuplicate_obj); $i++) {
+
+        if ($resultsDuplicate_obj[$i]->getDistrito() == $provinciaSelected) {
+            $resultsFiltrados[] = $resultsDuplicate_obj[$i];
+        }
+
+    }
+    return $resultsFiltrados;
+}
+
+function Escanyos($provinciaSelected)
+{
+    global $results_obj;
+    global $provincias_obj;
+    global $resultsDuplicate_obj;
+    $resultadosFiltrados = ResultsFiltrados($resultsDuplicate_obj, $provinciaSelected);
+    $delegados = Delegados($provincias_obj, $provinciaSelected);
+
+    //Sacar el minimo de votos
+    $resultVotos=[];
+    for ($i = 0; $i < count($resultadosFiltrados); $i++) {
+
+        $resultVotos[]=$resultadosFiltrados[$i]->getVotos();
+
+    }
+    $votosMinimos=array_sum($resultVotos)*0.03;
+
+    $posicion=0;
+    $escanyos=[];
+
+    // Escanyos todos a 0
+    for ($j = 0; $j < count($resultVotos); $j++) {
+        $escanyos[]=0;
+    }
+
+
+    for ($k = 0; $k < $delegados; $k++) {
+        $mayor = 0;
+        for ($i = 0; $i < count($resultadosFiltrados); $i++) {
+            if ($resultadosFiltrados[$i]->getVotos() > $votosMinimos && $resultVotos[$i] > $mayor) {
+
+                $mayor=$resultVotos[$i];
+                $posicion= $i;
+
+            }
+
+        }
+    $escanyos[$posicion]+=1;
+    $resultVotos[$posicion]= $resultadosFiltrados[$posicion]->getVotos() / ($escanyos[$posicion]+1);
+
+    }
+    $posicion=0;
+
+    for($i=0;$i<count($results_obj);$i++){
+
+        if($results_obj[$i]->getDistrito() == $provinciaSelected) {
+
+            $results_obj[$i]->setEscanyos($escanyos[$posicion]);
+            $posicion++;
+        }
+
+    }
+
+}
+
+
+
+function Generales($results_obj,$partidos_obj){
+
+
+    $resultsVotos=[];
+    $resultsEscanyos=[];
+
+    for($i=0;$i<count($results_obj);$i++){
+    Escanyos($results_obj[$i]->getDistrito());
+        for($k=0;$k<count($partidos_obj);$k++){
+
+            if($results_obj[$i]->getPartidos() == $partidos_obj[$k]->getName()){
+
+                $resultsEscanyos[$k]+=$results_obj[$i]->getEscanyos();
+                $resultsVotos[$k]+=$results_obj[$i]->getVotos();
+
+                $partidos_obj[$k]->setVotos($resultsVotos[$k]);
+                $partidos_obj[$k]->setEscanyos($resultsEscanyos[$k]);
+
+            }
+
+        }
+    }
+
+
+}
+
 
 
 ?>
-<html lang="es">
-<head>
-    <title>Elecciones</title>
+    <html lang="es">
+    <head>
+        <title>Elecciones</title>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+                crossorigin="anonymous"></script>
 
 
-</head>
-<body>
-<form method="get" action="main.php">
-    <select name="sortingCriteria">
-        <option selected value="unsorted">Selecciona filtrado</option>
-        <option selected value="Resultados_generales">Resultados generales</option>
-        <option selected value="Filtrar_por_provincia">Filtrar por provincia</option>
-        <!--Debo hacer que cuando clique en filtrar por provincia salga otro selector con las provincias -->
-        <option selected value="Filtrar_por_partido">Filtrar por partido</option>
-        <!--Debo hacer que cuando clique en filtrar por partidos salga otro selector con los partidos -->
-    </select>
-    <button type="submit">Sort</button>
-</form>
-<div>
-    <div class="row">
+    </head>
+    <body>
+    <form method="get" action="main.php">
+        <select name="sortingCriteria">
+            <option selected value="unsorted">Selecciona filtrado</option>
+            <option selected value="Resultados_generales">Resultados generales</option>
 
-        <?php
+            <option selected value="Filtrar_por_provincia">Filtrar por provincia</option>
+            <option selected value="Filtrar_por_partido">Filtrar por partido</option>
+            <!--Debo hacer que cuando clique en filtrar por partidos salga otro selector con los partidos -->
+        </select>
+        <button type="submit">Sort</button>
+    </form>
 
-        /*Escanyos();*/
-
-        ?>
-
-    </div>
-</div>
-
-</body>
-</html>
+    </body>
+    </html>
 <?php
 
-if (isset($_GET["sortingCriteria"]) || isset($_GET["provincia"])) {
+if (isset($_GET["sortingCriteria"]) || isset($_GET["provincia"]) || isset($_GET["partido"])) {
     //TODO: Logic to call a function depending on the sorting criteria.
-global $provincias_obj;
+    global $provincias_obj;
+
+    if ($_GET["sortingCriteria"] == "Resultados_generales") {
+        Generales($results_obj,$partidos_obj);
+        //Mirar bien, lo puse, pero no esta comprobado su funcionamiento, tocar tabla para mostrar general
+        tabla($results_obj);
+    }
+
     if ($_GET["sortingCriteria"] == "Filtrar_por_provincia") {
         echo '<form method="get" action="main.php">';
         echo '<select name="provincia">';
@@ -203,13 +240,15 @@ global $provincias_obj;
         echo '</form>';
     }
 
-        for ($i = 0; $i < count($provincias_obj); $i++) {
-            if ($_GET["provincia"] == $provincias_obj[$i]->getName()) {
-                // meter el array con las circumscripciones ya hechas
-                tabla($provincias_obj[$i]->getName());
-                break;
-            }
+    for ($i = 0; $i < count($results_obj); $i++) {
+        if ($_GET["provincia"] == $results_obj[$i]->getDistrito()) {
+            // meter el array con las circumscripciones ya hechas
+            Escanyos($results_obj[$i]->getDistrito());
+            tabla($results_obj[$i]->getDistrito(),$results_obj);
+            break;
         }
+    }
+
 
     /*for ($k = 0; $k < count($provincias_obj); $k++) {
         echo $_GET["devolverProvincias"];*/
@@ -218,3 +257,4 @@ global $provincias_obj;
 }
 
 ?>
+
