512a9b3b4214c648100d6c58d95794a403b3462e
erik <emendezmartin@iessonferrer.net>
2021-11-08 15:51:30 +0100

    sdf

--- EMendez/Tema2_POO/Elecciones/main.php
+++ EMendez/Tema2_POO/Elecciones/main.php
@@ -1,31 +1,30 @@
 <?php
-
 require("Partidos.php");
 require("Provincias.php");
 require("Resultados.php");
 error_reporting(0);
+
 $api_url = "https://dawsonferrer.com/allabres/apis_solutions/elections/api.php?data=";
 
 $partidos = json_decode(file_get_contents($api_url . "parties"), true);
 $provincias = json_decode(file_get_contents($api_url . "districts"), true);
 $resultados = json_decode(file_get_contents($api_url . "results"), true);
 
+
 /*echo "<br>";
 echo "<pre>";
 var_dump($provincias);
 echo "</pre>";*/
 
-// https://es.wikipedia.org/wiki/Sistema_D%27Hondt#:~:text=El%20n%C3%BAmero%20de%20votos%20recibidos,hasta%20que%20estos%20se%20agoten.
-// https://es.wikipedia.org/wiki/Circunscripciones_electorales_del_Congreso_de_los_Diputados
-
-//  Creo array para introducir los valores al objeto(casting)
-$results_obj = [];
 //con foreach le digo que el array es como el valor que le doy y me guarda en este ultimo
 // los valores del array.
 //Resultados
+//  Creo array para introducir los valores al objeto(casting)
+$results_obj = [];
 foreach ($resultados as $resultado) {
-    $results_obj[] = new Resultados($resultado["district"], $resultado["party"], $resultado["votes"]);
+    $results_obj[] = new Resultados($resultado["district"], $resultado["party"], $resultado["votes"],0);
 }
+
 
 //Creo array para introducir los valores al objeto(casting)
 // Partidos
@@ -36,16 +35,55 @@ foreach ($partidos as $partido) {
 
 //Creo array para introducir los valores al objeto(casting)
 // Provincias
-$provincias_obj = [];
 
+$provincias_obj = [];
 foreach ($provincias as $provincia => $valor) {
     $provincias_obj[] = new Provincias($valor["id"], $valor["name"], $valor["delegates"]);
 }
-$divisor = 0;
 
-// De una provincia cojo el partido que mas votos tiene, a este lo divido entre dos y le sumo un escaño, despues miro quien es el que mas votos tiene y hago la misma operación, si
-// ya tiene un escaño pero sigue siendo el que mas votos tiene dividir entre 3 y sumar otro escaño... asi sucesivamente. Hasta repartir todas las delegaciones en cada provincia.
-// problema no se como automatizar esto que acabo de escribir.
+//Duplicar objeto results, para trabajar en el duplicado e ir dividiendo los votos para sacar los escaños despues pasar los escaños al original
+$resultsDuplicate_obj = $results_obj;
+function tabla($resultSelector, $objUtilizado)
+{
+
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Circumscripción</th>";
+    echo "<th>Partido</th>";
+    echo "<th>Votos</th>";
+    echo "<th>Escaños</th>";
+    echo "</tr>";
+    echo "<tr>";
+
+    for ($i = 0; $i < count($objUtilizado); $i++) {
+
+        if ($resultSelector == "Resultados_generales") {
+            echo "<tr>";
+            echo "<td> Generales </td>";
+            echo "<td>" . $objUtilizado[$i]->getName() . "</td>";
+            echo "<td>" . $objUtilizado[$i]->getVotos() . "</td>";
+            echo "<td>" . $objUtilizado[$i]->getEscanyos() . "</td>";
+            echo "</tr>";
+        } elseif ($objUtilizado[$i]->getDistrito() == $resultSelector) {
+            echo "<tr>";
+            echo "<td>" . $objUtilizado[$i]->getDistrito() . "</td>";
+            echo "<td>" . $objUtilizado[$i]->getPartidos() . "</td>";
+            echo "<td>" . $objUtilizado[$i]->getVotos() . "</td>";
+            echo "<td>" . $objUtilizado[$i]->getEscanyos() . "</td>";
+            echo "</tr>";
+        }elseif ($objUtilizado[$i]->getPartidos() == $resultSelector) {
+            echo "<tr>";
+            echo "<td>" . $objUtilizado[$i]->getDistrito() . "</td>";
+            echo "<td>" . $objUtilizado[$i]->getPartidos() . "</td>";
+            echo "<td>" . $objUtilizado[$i]->getVotos() . "</td>";
+            echo "<td>" . $objUtilizado[$i]->getEscanyos() . "</td>";
+            echo "</tr>";
+        }
+
+    }
+    echo "</table>";
+}
+
 
 //Funcion para sacar delegaciones
 function Delegados($provincias_obj, $provinciaSelected)
@@ -59,72 +97,127 @@ function Delegados($provincias_obj, $provinciaSelected)
 
 }
 
-function Escanyos()
+//Filtrar resultados por provincia seleccionada
+function ResultsFiltrados($resultsDuplicate_obj, $provinciaSelected)
 {
-    // Como hago para poder saber que escaños he dado a cada partido,
-    // vale guardo los escaños en un array, el que mas tenga sera el primero
-    global $results_obj;
-    global $provincias_obj;
-    $escanyo = [];
+    $resultsFiltrados = [];
+    for ($i = 0; $i < count($resultsDuplicate_obj); $i++) {
 
-    //Ordeno los votos de mayor a menor,
-    for ($k = 0; $k < count($results_obj); $k++) {
-        for ($i = $k; $i < count($results_obj); $i++) {
-            if ($results_obj[$k]->getVotos() < $results_obj[$i]->getVotos()) {
-                $aux = $results_obj[$k];
-                $results_obj[$k] = $results_obj[$i];
-                $results_obj[$i] = $aux;
+        if ($resultsDuplicate_obj[$i]->getDistrito() == $provinciaSelected) {
+            $resultsFiltrados[] = $resultsDuplicate_obj[$i];
+        }
+
+    }
+    return $resultsFiltrados;
+}
+
+function Escanyos($provinciaSelected,$results_obj)
+{
+
+    global $provincias_obj;
+    global $resultsDuplicate_obj;
+    $resultadosFiltrados = ResultsFiltrados($resultsDuplicate_obj, $provinciaSelected);
+    $delegados = Delegados($provincias_obj, $provinciaSelected);
+
+    //Sacar el minimo de votos
+    $resultVotos = [];
+    for ($i = 0; $i < count($resultadosFiltrados); $i++) {
+
+        $resultVotos[] = $resultadosFiltrados[$i]->getVotos();
+
+    }
+    $votosMinimos = array_sum($resultVotos) * 0.03;
+
+    $posicion = 0;
+    $escanyos = [];
+
+    // Escanyos todos a 0
+    for ($j = 0; $j < count($resultVotos); $j++) {
+        $escanyos[] = 0;
+    }
+
+    for ($k = 0; $k < $delegados; $k++) {
+        $mayor = 0;
+        for ($i = 0; $i < count($resultadosFiltrados); $i++) {
+            if ($resultadosFiltrados[$i]->getVotos() > $votosMinimos && $resultVotos[$i] > $mayor) {
+
+                $mayor = $resultVotos[$i];
+                $posicion = $i;
+
+            }
+
+        }
+        $escanyos[$posicion] += 1;
+        $resultVotos[$posicion] = $resultadosFiltrados[$posicion]->getVotos() / ($escanyos[$posicion] + 1);
+
+    }
+    $posicion = 0;
+
+    for ($i = 0; $i < count($results_obj); $i++) {
+
+        if ($results_obj[$i]->getDistrito() == $provinciaSelected) {
+
+            $results_obj[$i]->setEscanyos($escanyos[$posicion]);
+            $posicion++;
+        }
+
+    }
+
+    return $results_obj;
+
+}
+
+function Generales($results_obj, $partidos_obj)
+{
+
+    $resultsVotos = [];
+    $resultsEscanyos = [];
+
+    for ($i = 0; $i < count($results_obj); $i++) {
+        Escanyos($results_obj[$i]->getDistrito(),$results_obj);
+        for ($k = 0; $k < count($partidos_obj); $k++) {
+
+            if ($results_obj[$i]->getPartidos() == $partidos_obj[$k]->getName()) {
+
+                $resultsEscanyos[$k] += $results_obj[$i]->getEscanyos();
+                $resultsVotos[$k] += $results_obj[$i]->getVotos();
+
+                $partidos_obj[$k]->setVotos($resultsVotos[$k]);
+                $partidos_obj[$k]->setEscanyos($resultsEscanyos[$k]);
+
             }
         }
     }
 
-    $divisor = 2;
-    $escanyo = 0;
-    // Intento hacer lo de los escaños en mi cabeza y he pensado esto, el tope de veces que se repetira
-    // el bucle seran las diferentes delegaciones que tiene cada provincia, y digo yo vale
-    // despues si en "resultados" esta en la posicion significa que es el que tiene mas votos
-    // pues se divide entre dos y le damos un escaño. El punto viene que como yo lo tengo hecho
-    // me sale un desplegable con todas las provincias y al seleccionarme una deberia hacerlo
-    // de esa provincia, problema no se como enlazarlo, por que claro yo he ordenado los votos
-    // de todo el pais, no dividiendo cada provincia por el partido que mas votos tiene.
-
 }
 
-function tabla($resultadosProvincias)
-{
-    global $results_obj;
-    echo "<table>";
-    echo "<tr>";
-    echo "<th>Circumscripción</th>";
-    echo "<th>Partido</th>";
-    echo "<th>Votos</th>";
-    echo "<th>Escaños</th>";
-    echo "</tr>";echo "<tr>";
-    for ($i = 0; $i < count($results_obj); $i++) {
+function EscanyosPartido($results_obj){
 
-        if ($results_obj[$i]->getDistrito() == $resultadosProvincias) {
-            echo "<tr>";
-            echo "<td>" . $results_obj[$i]->getDistrito() . "</td>";
-            echo "<td>" . $results_obj[$i]->getPartidos() . "</td>";
-            echo "<td>" . $results_obj[$i]->getVotos() . "</td>";
-            echo "</tr>";
-        }
-
+    global $provincias_obj;
+    for($i=0;$i<count($provincias_obj);$i++){
+        Escanyos($provincias_obj[$i]->getName(),$results_obj);
     }
-    echo "</table>";
+    return $results_obj;
 }
-
 
 ?>
 <html lang="es">
 <head>
     <title>Elecciones</title>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-            crossorigin="anonymous"></script>
-
-
+    <style>
+        th{
+            background-color: black;
+            color:white;
+            border:1px solid white;
+            border-collapse: collapse;
+            padding: 10px;
+        }
+        table,tr,td{
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 10px;
+        }
+    </style>
 </head>
 <body>
 <form method="get" action="main.php">
@@ -132,31 +225,24 @@ function tabla($resultadosProvincias)
         <option selected value="unsorted">Selecciona filtrado</option>
         <option selected value="Resultados_generales">Resultados generales</option>
         <option selected value="Filtrar_por_provincia">Filtrar por provincia</option>
-        <!--Debo hacer que cuando clique en filtrar por provincia salga otro selector con las provincias -->
         <option selected value="Filtrar_por_partido">Filtrar por partido</option>
-        <!--Debo hacer que cuando clique en filtrar por partidos salga otro selector con los partidos -->
     </select>
     <button type="submit">Sort</button>
 </form>
-<div>
-    <div class="row">
-
-        <?php
-
-        /*Escanyos();*/
-
-        ?>
-
-    </div>
-</div>
 
 </body>
 </html>
 <?php
 
-if (isset($_GET["sortingCriteria"]) || isset($_GET["provincia"])) {
+if (isset($_GET["sortingCriteria"]) || isset($_GET["provincia"]) || isset($_GET["partido"])) {
     //TODO: Logic to call a function depending on the sorting criteria.
-global $provincias_obj;
+
+
+    if ($_GET["sortingCriteria"] == "Resultados_generales") {
+        Generales($results_obj, $partidos_obj);
+        tabla($_GET["sortingCriteria"], $partidos_obj);
+    }
+
     if ($_GET["sortingCriteria"] == "Filtrar_por_provincia") {
         echo '<form method="get" action="main.php">';
         echo '<select name="provincia">';
@@ -171,18 +257,38 @@ global $provincias_obj;
         echo '</form>';
     }
 
-        for ($i = 0; $i < count($provincias_obj); $i++) {
-            if ($_GET["provincia"] == $provincias_obj[$i]->getName()) {
-                // meter el array con las circumscripciones ya hechas
-                tabla($provincias_obj[$i]->getName());
-                break;
-            }
+    for ($i = 0; $i < count($results_obj); $i++) {
+        if ($_GET["provincia"] == $results_obj[$i]->getDistrito()) {
+
+            Escanyos($results_obj[$i]->getDistrito(),$results_obj);
+            tabla($results_obj[$i]->getDistrito(), $results_obj);
+            break;
+        }
+    }
+    if ($_GET["sortingCriteria"] == "Filtrar_por_partido") {
+        echo '<form method="get" action="main.php">';
+        echo '<select name="partido">';
+
+        //Automatizar las selecciones con unos bucles.
+        foreach ($partidos_obj as $partido => $valores) {
+            echo "<option selected value='" . $valores->getName() . "'>" . $valores->getName() . "</option> ";
         }
 
-    /*for ($k = 0; $k < count($provincias_obj); $k++) {
-        echo $_GET["devolverProvincias"];*/
+        echo '</select>';
+        echo '<button  type="submit">Sort2</button>';
+        echo '</form>';
+    }
 
-    //}
+    for ($i = 0; $i < count($results_obj); $i++) {
+
+        if ($_GET["partido"] == $results_obj[$i]->getPartidos()) {
+
+            $escanyosPartidos = EscanyosPartido($results_obj);
+            tabla($results_obj[$i]->getPartidos(), $escanyosPartidos);
+            break;
+        }
+    }
+
 }
 
 ?>
