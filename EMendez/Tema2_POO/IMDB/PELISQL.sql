20e5feceed4eef24c91e3cfcc0637d704b202145
EriKoT <erik55@outlook.es>
2021-11-20 20:49:01 +0100

    dfg

---
+++ EMendez/Tema2_POO/IMDB/PELISQL.sql
@@ -0,0 +1,2 @@
+INSERT INTO Peliculas (ID,Nombre,IMG,Trailer,Genero,Duracion,Fecha_Salida,Calificacion,Sinopsis)
+values(1,"Sin tiempo para morir","https://pics.filmaffinity.com/no_time_to_die-525355918-large.jpg","https://www.youtube.com/watch?v=c933JBJhKZE","");
