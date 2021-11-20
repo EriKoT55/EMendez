34d821441b3ce4ab0522e1ba686352b6087268fc
EriKoT <erik55@outlook.es>
2021-11-20 20:44:46 +0100

    dfg

--- EMendez/Tema2_POO/IMDB/BD.php
+++ EMendez/Tema2_POO/IMDB/BD.php
@@ -1,5 +1,19 @@
 <?php
 
+/*Crear una nueva conexion en MYSQL, CASA*/
+$servername="sql480.main-hosting.eu";//sql480.main-hosting.eu
+$username="u850300514_emendez"; //u850300514_emendez //casa erikPhp // clase root
+$password="x43233702G";//x43233702G
+$database="u850300514_emendez";//RickMorthy_u850300514_emendez
+
+//Creo la conexion
+$conn = new mysqli($servername,$username,$password,$database);
+
+// Me aseguro de si va bien la conexion
+if($conn->connect_error){
+    die("Conexion fallida: ". $conn->connect_error);
+}
+
 
 
 ?>
